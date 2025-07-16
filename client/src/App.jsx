7e2ee9e411fcdda14dd0bc8935c35bc6f898a6d8
MD. SAHIL <codewithkinu@gmail.com>
2025-07-16 17:56:53 +0530

    feat:services pages added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,36 +8,46 @@ import MyAppointment from './pages/MyAppointment'
 import MyProfile from './pages/MyProfile'
 import Doctors from './pages/Doctors'
 import Appointment from './pages/Appointment'
+import Services from './pages/Services' // Import the new Services component
 import Navbar from './components/Navbar'
-import Header from './components/Header'
 import Footer from './components/Footer'
-import {toast,ToastContainer} from 'react-toastify'
-
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 const App = () => {
   return (
-    <div className='mx-[5%]  md:mx-[10%]'>
-      <Navbar/>
-      <ToastContainer/>
-      <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/about' element={<About/>}/>
-      <Route path='/contact' element={<Contact/>}/>
-      <Route path='/login' element={<Login/>}/>
-      <Route path='/my-appointment' element={<MyAppointment/>}/>
-      <Route path='/appointment/:docId' element={<Appointment/>}/>
-      <Route path='/my-profile' element={<MyProfile/>}/>
-      <Route path='/doctors' element={<Doctors/>}/>
-      <Route path='/doctors/:speciality' element={<Doctors/>}/>
-
-
-    </Routes>
-    <Footer/>
-    
-
-    </div>
-    
-    
+    <>
+      <Navbar />
+      <ToastContainer 
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+      <div className='min-h-screen flex flex-col'>
+        <main className='flex-grow mx-[5%] md:mx-[5%] py-4'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/contact' element={<Contact />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/my-appointment' element={<MyAppointment />} />
+            <Route path='/appointment/:docId' element={<Appointment />} />
+            <Route path='/my-profile' element={<MyProfile />} />
+            <Route path='/doctors' element={<Doctors />} />
+            <Route path='/doctors/:speciality' element={<Doctors />} />
+            <Route path='/services' element={<Services />} /> {/* New Services route */}
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </>
   )
 }
 
